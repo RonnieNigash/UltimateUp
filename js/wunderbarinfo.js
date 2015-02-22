9b5114ac38c1b669296236cd1ae749342a36bb62
hotwinter <xiaoba2008@gmail.com>
2015-02-22 15:58:39 -0800

    stupid git

--- js/wunderbarinfo.js
+++ js/wunderbarinfo.js
@@ -6,8 +6,6 @@ relayr.devices().getDeviceData({
   deviceId: "c4e8751d-f9ff-4509-ba0c-98b78afb0576",
   token: "_C4efSMSUjtisJQybuiC5PN_pkVW.P-S" ,
   incomingData: function(data){
-    console.log("sensor",data);
-    console.log(data.ts);
     var roll = data.gyro.x;
     var x = document.getElementById("roll");
     x.setAttribute("value", roll);
@@ -16,7 +14,24 @@ relayr.devices().getDeviceData({
     y.setAttribute("value", pitch);
     var yaw = data.gyro.z;
     var z = document.getElementById("yaw");
-    z.setAttribute("value", yaw);
-    console.log(data.accel);
+    z.setAttribute("value", yaw)
+    x = data.accel.x;
+    y = data.accel.y;
+    z = data.accel.z;
+    /*var accelx = document.getElementById("acclex");
+    var accely = document.getElementById("accley");
+    var accelz = document.getElementById("acclez");
+    accelx.setAttribute("value", x);
+    accely.setAttribute("value", y);
+    accelz.setAttribute("value", z);
+    */
+  }
+});
+
+relayr.devices().getDeviceData({
+  deviceId: "2ae2d3c9-3491-4d7c-beca-602d206d4ad8",
+  token: "_C4efSMSUjtisJQybuiC5PN_pkVW.P-S" ,
+  incomingData: function(data){
+    console.log(data);
   }
 });
